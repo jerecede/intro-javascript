3f634d605e5684791b42123d762d0a33d7dc49dd
jerecede <jeremias10458@gmail.com>
2025-02-17 11:10:42 +0100

    boolean update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 console.log("hello world");
 console.log("ciao mondo");
 
+//ci sono operatori binari e non binari
 //typeof, operatore che mi dice la tipologia
 console.log(typeof 5); //number
 console.log(typeof "hello world"); //string
@@ -23,6 +24,7 @@ console.log(0/0); //esce NaN
 
 console.log("somma 5 + 7", 5 + 7, "yeeee");
 console.log("sottrazione 5 - 7", 5 - 7);
+console.log("negativo - 7", - 7);
 console.log("prodotto 5 * 7", 5 * 7);
 console.log("divisione 5 / 7", 5 / 7);
 console.log("potenza 2**5, 2 alla quinta", 2**5);
@@ -37,7 +39,7 @@ console.log('arrotondamento 9.6', Math.round(9.6)); //Math.floor difetto, Math.c
 
 //STRING
 
-console.log("234", 234); //stringa in bianco, numero in verde
+console.log("234", 234); //stringa in bianco, numero e boolean in verde
 console.log("io sono una stringa");
 console.log('io sono una stringa'); //non cambia
 console.log('io sono una "stringa"');
@@ -51,3 +53,24 @@ console.log(`"l'apostrofo" è importante
 console.log(`la somma di 5 e 7 è ${7 + 5}`); //può fare le operazioni dentro
 
 console.log('ciao a tutti' + ' ' + 'mi chiamo pippo'); //concatena le stringhe
+
+//BOOLEAN
+
+console.log("vero", true);
+console.log("falso", false);
+
+//not
+console.log("not vero", !true);
+console.log("not falso", !false);
+
+//and
+console.log("true and true", true && true);
+console.log("false and true", false && true);
+console.log("true and false", true && false);
+console.log("false and false", false && false);
+
+//or
+console.log("true or true", true || true);
+console.log("false or true", false || true);
+console.log("true or false", true || false);
+console.log("false or false", false || false);
