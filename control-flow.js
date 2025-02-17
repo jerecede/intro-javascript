0b5109041b4a4604afd1be7f08e5c3310bac3740
jerecede <jeremias10458@gmail.com>
2025-02-17 13:30:22 +0100

    update

--- control-flow.js
+++ control-flow.js
@@ -10,10 +10,10 @@
 // alert("ciao " + userName);
 //risponde con una finestra, non con console
 
-//IF
+//IF CONDIZIONE
 
-const userAgeString = prompt("quanti anni hai?");
-const userAgeNumber = parseInt(userAgeString);
+// const userAgeString = prompt("how old are you?");
+// const userAgeNumber = parseInt(userAgeString);
 
 // if(parseInt(userAge) >= 18){ //parseInt perchè il prompt prende stringhe, anche se c'è la conversione forzata, parse è più corretto
 //     console.log('benvenuto nel sito di incontri piccanti');
@@ -22,12 +22,64 @@ const userAgeNumber = parseInt(userAgeString);
 //     console.log('va via!!!');
 // }
 
-if(userAgeNumber >= 18 && userAgeNumber <= 80){
-    console.log('benvenuto nel sito di incontri piccanti');
+// if(userAgeNumber >= 18 && userAgeNumber <= 80){
+//     console.log('benvenuto nel sito di incontri piccanti');
+// }else if(userAgeNumber < 18){
+//     console.log('va via novellino!!!');
+// }else{
+//     console.log('il cuore potrebbe non reggere');
+// }
+
+//CICLO WHILE
+
+let i = 0;
+//iterator, variabile di controllo
+while (i < 11) {
+    console.log(i);
+    i++; // i = i + 1, zucchero sintattico
 }
-else if(userAgeNumber < 18){
-    console.log('va via novellino!!!');
+console.log("ho finito il ciclo while");
+
+let j = 0;
+while (j < 11) {
+    console.log(j);
+    j += 2; // j = j + 2
 }
-else{
-    console.log('il cuore potrebbe non reggere');
+console.log("ho finito il ciclo while");
+
+let k = 10;
+while (k >= 0) {
+    console.log(k);
+    k -= 2;
+    //oppure k--
 }
+console.log("ho finito il ciclo while");
+
+//calcola i quadrati da 0 a 10
+let q = 0;
+while (q <= 10) {
+    const square = i ** 2;
+    console.log(square);
+    q++;
+}
+console.log("ho finito il ciclo while");
+
+//ciclo infinito
+const secretNumber = Math.round(Math.random() * 10) //Math.random da 0 a 1 float, bisogna arrotondare
+while(true){
+    const guessedString = prompt("indovina il numero che sto pensando (da 0 a 10)...");
+    const guessedNumber = parseInt(guessedString);
+    if (guessedNumber === secretNumber) {
+        alert("bravo, sei un campione!!!");
+        break; //esce dal ciclo
+    } else {
+        alert("schiappa!!!");
+    }
+}
+
+//CICLO DO WHILE
+
+//CICLO FOR
+
+
+
