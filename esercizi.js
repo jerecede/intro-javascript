029e8aad77ff0f88b1cfa1e7c2ed43ee42272b96
jerecede <jeremias10458@gmail.com>
2025-02-18 10:57:12 +0100

    update piramide

--- esercizi.js
+++ esercizi.js
@@ -1,5 +1,14 @@
 let piramide = "";
-for (let i = 0; i < 7; i++) {
-    piramide += "#";
-    console.log(piramide);
+let altezza = parseInt(prompt("quanto righe vuoi nella piramide"));
+if(altezza < Infinity && altezza > 0) {
+    for (let i = 0; i < altezza; i++) {
+        piramide += "#";
+        console.log(piramide);
+    }
+    console.log("sta piramide è alta " + altezza + " righe");
 }
+else{
+    alert("devi inserire un numero, e maggiore di 0 possibilmente, ignorante...");
+}
+
+//NaN non puoi compararlo
