63bfee2658da2150a210b0d0759fefade51635a1
jerecede <jeremias10458@gmail.com>
2025-02-26 12:03:54 +0100

    reduce

--- high-order-functions.js
+++ high-order-functions.js
@@ -2,7 +2,7 @@ const testArray = [2, 5, 7, 1, 0, 100, 103];
 
 const testArray2 = ['pippo', 'pluto', 'paperino', 'clarabella', 'minnie'];
 
-function add2ToArray(arrayNumbers){
+function add2ToArray(arrayNumbers) {
     let newArray = [];
 
     for (let i = 0; i < arrayNumbers.length; i++) {
@@ -17,7 +17,7 @@ function add2ToArray(arrayNumbers){
 const arrayp = add2ToArray(testArray);
 console.log(arrayp);
 
-function addIndexToArray(arrayNumbers){
+function addIndexToArray(arrayNumbers) {
     let newArray = [];
 
     for (let i = 0; i < arrayNumbers.length; i++) {
@@ -35,51 +35,51 @@ console.log(arrayp2);
 //facciamo
 //FUNZIONE MAP
 
-function map(array, mappingFunction){ //array anche di non numeri, e una funzione come parametro
+function map(array, mappingFunction) { //array anche di non numeri, e una funzione come parametro
     let newArray = [];
 
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
-        const newElement = mappingFunction(element, i, array);
+        const newElement = mappingFunction(element, i, array); //massimo parametri
         newArray.push(newElement);
     }
 
     return newArray;
 }
 
-function add2(number){
+function add2(number) {
     const newNumber = number + 2;
     return newNumber;
 }
 
-function addIndex(number, index){
+function addIndex(number, index) {
     const newNumber = number + index;
     return newNumber;
 }
 
 // const arrayp3 = map(testArray, add2);
 const arrayp3 = testArray.map(add2);
-console.log(arrayp3);
+console.log('MAP add2\n', arrayp3);
 
 // const arrayp4 = map(testArray, addIndex);
 const arrayp4 = testArray.map(addIndex);
-console.log(arrayp4);
+console.log('MAP addIndex\n', arrayp4);
 
-function toUpperCase(str){
+function toUpperCase(str) {
     const newStr = str.toUpperCase();
     return newStr;
 }
 
 // const arrayp5 = map(testArray2, toUpperCase);
 const arrayp5 = testArray2.map(toUpperCase);
-console.log(arrayp5);
+console.log('MAP toUpperCase\n', arrayp5);
 
 //scriverlo in una solo linea
 const arrayp6 = testArray.map(number => number + 3); //const add3 = number => number + 3, invece faccio funzione anonima, diretto
-console.log(arrayp6);
+console.log('MAP .reduce add3\n', arrayp6);
 
-const arrayp7 = testArray.map((number,index) => number - (index*2)); //con due parametri si usano ()
-console.log(arrayp7);
+const arrayp7 = testArray.map((number, index) => number - (index * 2)); //con due parametri si usano ()
+console.log('MAP .reduce addIndex\n', arrayp7);
 
 const arrayp8 = testArray2.map(str => str.length); //restituiscd array con le lunghezze di ogni stringa
 console.log(arrayp8);
@@ -95,9 +95,9 @@ function keepEven(arrayNumbers) {
     for (let i = 0; i < arrayNumbers.length; i++) {
         const num = arrayNumbers[i];
 
-        if(num % 2 === 0){
+        if (num % 2 === 0) {
             newArray.push(num);
-        }        
+        }
     }
 
     return newArray;
@@ -106,23 +106,23 @@ function keepEven(arrayNumbers) {
 const arrayp10 = keepEven(testArray);
 console.log(arrayp10);
 
-function filter(array, filteringFunction){
+function filter(array, filteringFunction) {
     let newArray = [];
 
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
 
-        if(filteringFunction(element, i, array)){
+        if (filteringFunction(element, i, array)) { //massimo parametri
             newArray.push(element);
-        }        
+        }
     }
 
     return newArray;
 }
 
-function isEven(numb){
-    if(numb % 2 === 0) {return true}
-    else {return false}
+function isEven(numb) {
+    if (numb % 2 === 0) { return true }
+    else { return false }
 }
 
 // const arrayp11 = filter(testArray, isEven);
@@ -131,3 +131,54 @@ console.log(arrayp11);
 
 const arrayp12 = testArray.filter(num => num < 5);
 console.log(arrayp12);
+
+const arrayp13 = testArray2.filter(str => str.length > 6);
+console.log(arrayp13);
+
+// const arrayp14 = testArray2.filter((str, index) => index % 2 !== 0);
+const arrayp14 = testArray2.filter((_, index) => index % 2 !== 0); //anche se uso solo index devo mettere i due parametri, uso underscore
+console.log(arrayp14);
+
+//FUNZIONE REDUCE
+
+function sumAll(arrayNumbers){
+    let accumulator = 0; //puo essere array, stringa, number
+
+    for (let i = 0; i < arrayNumbers.length; i++) {
+        const num = arrayNumbers[i];
+
+        accumulator = accumulator + num;
+    }
+
+    return accumulator;
+}
+
+const arrayp15 = sumAll(testArray);
+console.log(arrayp15);
+
+function reduce(array, reducingFunction, startingAccumulator){
+    let accumulator = startingAccumulator;
+
+    for (let i = 0; i < array.length; i++) {
+        const current = array[i];
+
+        accumulator = reducingFunction(accumulator, current, i, array); //massimo parametri
+    }
+
+    return accumulator;
+}
+
+function sum(accumulator, current){
+    const newAccumulator = accumulator + current;
+    return newAccumulator;
+}
+
+const arrayp16 = reduce(testArray, sum, 0);
+console.log(arrayp16);
+
+// const arrayp17 = testArray.reduce(sum, 0);
+const arrayp17 = testArray.reduce((accumulator, current) => accumulator + current, 0);
+console.log(arrayp17);
+
+const arrayp18 = testArray2.reduce((a, c) => a + c, ""); // a = accumulator, c = current
+console.log(arrayp18);
